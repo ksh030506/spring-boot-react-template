5a48634bf1ce98cf467e85dd9c7f4e67d8fa2e8d
ksh030506 <ksh030506@naver.com>
2020-07-21 08:11:00 +0900

    Update index.tsx

--- frontend/src/main/js/index.tsx
+++ frontend/src/main/js/index.tsx
@@ -1,13 +1,8 @@
 import * as React from "react";
-import * as ReactDOM from 'react-dom';
-
-
+import * as ReactDOM from "react-dom";
 
 const render = () => {
-    ReactDOM.render(
-        <h1>Hello world</h1>,
-        document.getElementById("root")
-    );
+  ReactDOM.render(<h1>Hello</h1>, document.getElementById("root"));
 };
 
 export default render;
